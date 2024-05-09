d0b2614551e91bc2b738ca90db896c0f26271ab9
AR <aarokan@gmail.com>
2024-05-09 23:44:32 +0300

    declare the function and get the text from the user

--- readability.c
+++ readability.c
@@ -1,27 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// declare gradeIndex to use it with the functions  
+// declare gradeIndex to use it with the functions
 // declare calculateGrade function
+float calculateGrade(string text);
 // delcare printGrade function
 
 int main(void)
 {
     // get the text from the user
+    string text = get_string("Text: ");
 
     // call calculateGrade function
+    float gradeIndex = calculateGrade(text);
 
     // call printGrade function
 }
 
 
 // define function that calculate grade & output it
-    // declare wordCount, letterCount, lettersAvg, sentencesCount, sentencesAvg 
+float calculateGrade(string text)
+{
+    // declare wordsCount, lettersCount, lettersAvg, sentencesCount, sentencesAvg
+    int wordsCount, lettersCount, sentencesCount;
+    float lettersAvg, sentencesAvg;
     // loop through the text
         // increment letterCount
         // if there is no uppercase or lower case letter incerment wordCount
         // if there is (., !, ?) increment sentencesCount
     // calculate grade based on Coleman-Liau index = 0.0588 * L - 0.296 * S - 15.8
+}
 
 
 // define function that gets gradeIndex and prints grade level
